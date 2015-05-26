e842db210d2d4c7ed252758f1471073e7294514e
Thomas Reto Strub <ths@strubt.ch>
2015-05-26 23:07:30 +0200

    update usings

--- CodingSessionService/Program.cs
+++ CodingSessionService/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using c = System.Console;
 
 namespace BFH.NetDS.WebServices.CodingSession.Service {
 
